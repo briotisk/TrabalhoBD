bb4733db862477b9d54c7a1c9d40a358458f037e
briotisk <briotisk@gmail.com>
2023-12-08 08:36:28 -0300

    Arquivo dadod.sql criado

---
+++ dados.sql
@@ -0,0 +1,147 @@
+INSERT INTO PLANETA
+       VALUES
+            ('Terra', 'Solar', 'SIM'),
+            ('Júpiter', 'Solar', 'NAO'),
+            ('Titã', 'Solanus', 'SIM'),
+            ('Cybertron', 'Aguilar', 'NAO'),
+            ('Marte', 'Solar', 'SIM');
+
+INSERT INTO LUA
+       VALUES
+            ('Terra', 'Lua'),
+            ('Júpiter', 'Eoropa'),
+            ('Júpiter', 'Calisto'),
+            ('Júpiter', 'Io'),
+            ('Júpiter', 'Ganimedes'),
+            ('Marte', 'Fobos'),
+            ('Marte', 'Deimos');
+
+INSERT INTO NAVE
+       VALUES
+            ('HSL9D9S2', 381, 15000525, 120000),
+            ('LDV37C3F', 1032, 200500, 50000);
+
+INSERT INTO COLONIA
+       VALUES
+            (),
+            (),
+            ();
+
+INSERT INTO INSTALACAO
+       VALUES
+            (),
+            (),
+            ();
+
+INSERT INTO PESSOA
+       VALUES
+            (),
+            (),
+            (),
+            (),
+            (),
+            (),
+            (),
+            (),
+            (),
+            ();
+
+INSERT INTO ENGENHEIRO
+       VALUES
+            (),
+            ();
+
+INSERT INTO OPERARIO
+       VALUES
+            (),
+            ();
+
+INSERT INTO PROFISSIONAL_SAUDE
+       VALUES
+            (),
+            ();
+
+INSERT INTO PESQUISADOR
+       VALUES
+            (),
+            ();
+
+INSERT INTO BASE_MILITAR
+       VALUES
+            (),
+            ();
+
+INSERT INTO MILITAR
+       VALUES
+            (),
+            ();
+
+INSERT INTO PILOTO
+       VALUES
+            (),
+            ();
+
+INSERT INTO EXPLORADOR
+       VALUES
+            (),
+            ();
+
+INSERT INTO SOLDADO
+       VALUES
+            (),
+            ();
+
+INSERT INTO LABORATORIO
+       VALUES
+            (),
+            ();
+
+INSERT INTO PROJETO_PESQUISA
+       VALUES
+            (),
+            ();
+
+INSERT INTO HOSPITAL
+       VALUES
+            (),
+            ();
+
+INSERT INTO DEFENDE
+       VALUES
+            (),
+            ();
+
+INSERT INTO EXPLORA
+       VALUES
+            (),
+            ();
+
+INSERT INTO PROJETA_INSTALACAO
+       VALUES
+            (),
+            ();
+
+INSERT INTO CONSTROI_INSTALACAO
+       VALUES
+            (),
+            ();
+
+INSERT INTO TRABALHO_SAUDE
+       VALUES
+            (),
+            ();
+
+INSERT INTO ROTA
+       VALUES
+            (),
+            ();
+
+INSERT INTO VIAGEM
+       VALUES
+            (),
+            ();
+
+INSERT INTO VIAJAM
+       VALUES
+            (),
+            ();
