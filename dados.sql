979572535ed5f8a0e7642f24ecaf7cdff7dfe912
Guilherme Chiarotto <guichiarotto10@gmail.com>
2023-12-09 12:56:30 -0300

    a

--- dados.sql
+++ dados.sql
@@ -23,12 +23,12 @@ INSERT INTO NAVE
 
 INSERT INTO COLONIA
        VALUES
-            (123456789100,'Terra',123,123,'Nova Catanduva',1500),
-            (123456789101,'Terra',124,125,'Brasilio',2300),
-            (123456789102,'Marte',100,100,'Natalina',2321),
-	    (123456789103,'Titã',500,500,'Ararino',3500),	
-	    (123456789104,'Marte',700,700,'Campo Longo',4000),
-	    (123456789105,'Titã',800,800,'São Marcos',4500);
+            (123456789100,'Terra',-30,80,'Nova Catanduva',1500),
+            (123456789101,'Terra',50,170,'Brasilio',2300),
+            (123456789102,'Marte',10,120,'Natalina',2321),
+	    (123456789103,'Titã',-70,110,'Ararino',3500),	
+	    (123456789104,'Marte',-20,-170,'Campo Longo',4000),
+	    (123456789105,'Titã',60,-35,'São Marcos',4500);
 
 INSERT INTO INSTALACAO
        VALUES
