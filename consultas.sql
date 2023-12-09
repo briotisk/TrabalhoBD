1e66e221af3b4c85d2adbdcecb20285c9633124e
Caio Godinho <caio.godinho@wr.sc.usp.br>
2023-12-09 14:52:20 -0300

    adicionou-se a primeira consulta

--- consultas.sql
+++ consultas.sql
@@ -1,1 +1,6 @@
 -- Consultar todos os leitos disponíveis em uma colônia
+select c.id_colonia, count (*)
+from (colonia c join pessoa p on c.id_Colonia = p.colonia) where (Upper(profissao) = 'PROFISSIONAL DA SAUDE') 
+group by (c.id_Colonia)
+having count(*) > 0
+order by c.id_Colonia;
