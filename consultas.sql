879ad7576407e5bfa18738cb6a6eb908fd79856e
Arthur Reis <99774782+arthurb-reis@users.noreply.github.com>
2023-12-09 22:47:46 -0300

    Update consultas.sql

--- consultas.sql
+++ consultas.sql
@@ -4,3 +4,15 @@ from (colonia c join pessoa p on c.id_Colonia = p.colonia) where (Upper(profissa
 group by (c.id_Colonia)
 having count(*) > 1
 order by c.id_Colonia;
+
+
+SELECT col.id_colonia, col.nome, col.nrohabitantes/COUNT(ps.cpf) as ProfissionaisDaSaudePorHabitante
+FROM (profissional_saude ps JOIN pessoa p ON ps.cpf = p.cpf)
+    JOIN colonia col on p.colonia = col.id_colonia
+GROUP BY col.id_colonia
+    HAVING col.nrohabitantes/COUNT(p.cpf) =
+   	 (SELECT MAX(ProfissionaisDaSaudePorHabitante) FROM
+   		 (SELECT col.nrohabitantes/COUNT(p.cpf) as ProfissionaisDaSaudePorHabitante
+   			FROM (profissional_saude ps JOIN pessoa p ON ps.cpf = p.cpf)
+   				JOIN colonia col ON p.colonia = col.id_colonia
+   		 GROUP BY col.id_colonia) as taxa);
