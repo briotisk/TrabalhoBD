d330d01349d3c102567da8c1a4cd36173e46ae0f
FelipeCecato <fececato31@gmail.com>
2023-12-09 23:07:28 -0300

    consultas

--- consultas.sql
+++ consultas.sql
@@ -16,3 +16,28 @@ GROUP BY col.id_colonia
    			FROM (profissional_saude ps JOIN pessoa p ON ps.cpf = p.cpf)
    				JOIN colonia col ON p.colonia = col.id_colonia
    		 GROUP BY col.id_colonia) as taxa);
+
+SELECT c.id_colonia, c.nome, COUNT(p.cpf) as qtd_pessoas
+FROM colonia c JOIN pessoa p ON p.colonia = c.id_colonia
+GROUP BY c.id_colonia, c.nome
+HAVING COUNT (p.cpf) = ( SELECT MAX(qtds.qtd_pessoas)
+	FROM ( SELECT COUNT(p.cpf) as qtd_pessoas
+		FROM colonia c JOIN pessoa p ON p.colonia = c.id_colonia
+		GROUP BY c.id_colonia, c.nome) qtds);
+
+SELECT p.nome, c.nome, e.CREA, SUM(h.nroleitos) as qtd_leitos_projetados
+FROM ((((projeta_instalacao pj JOIN hospital h ON pj.instalacao = h.id_instalacao)
+	RIGHT JOIN engenheiro e ON pj.engenheiro = e.cpf)
+	JOIN pessoa p ON e.cpf = p.cpf)
+	JOIN colonia c ON c.id_colonia = p.colonia
+	)
+GROUP BY e.cpf, p.nome, c.nome, e. CREA;
+
+SELECT p.nome, p.profissao, c.Nome
+FROM pessoa p JOIN colonia c ON p.colonia = c.Id_colonia
+WHERE DataNasc in
+	(SELECT min(DataNasc)
+		FROM pessoa pi JOIN colonia ci ON ci.Id_Colonia = pi.Colonia
+		GROUP BY ci.Id_Colonia)
+GROUP BY p.nome, p.profissao, c.Nome
+ORDER BY c.Nome;
